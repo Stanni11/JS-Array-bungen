2c752fd1167eba7de4612590ae6a5de0596e5342
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-08-12 15:31:45 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,156 @@
+/* const blumen = ['Tulpen', 'Ahorn', 'rosen']
+blumen.push('rosen')
+blumen[1] = 'Tulpen'
+blumen.unshift(('Veilchen'))
+
+blumen.push('Ahorn')
+
+// blumen.pop()
+blumen.splice(3, 1)
+
+let pflanzen = [...blumen]
+pflanzen = [blumen]
+console.log(blumen)
+console.log(pflanzen)
+
+
+let x = 8
+let y = x
+
+y = 10
+
+console.log(x)
+console.log(y)
+ */
+/* let getraenke = [
+    "Coca-Cola",
+    "Apfelsaft",
+    "Pepsi",
+    "Traubensaft",
+    "Sprite",
+    "Orangensaft",
+    "Red Bull Energy Drink",
+    "Fanta"
+]
+
+let upperDrinks = getraenke.map((item) => {
+    return item.toLocaleUpperCase()
+})
+console.log(upperDrinks) */
+
+
+/* let arr1 = ['Straße', 'Postleitzahl', 'Ort', 'Bundesland']
+console.log(arr1) */
+
+/* let array = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+];
+let arr1 = array.map((item) => {
+
+    return item * 2
+
+})
+let arr2 = arr1.sort((function (a, b) {
+
+    return a - b
+}))
+
+console.log(arr2) */
+
+/* let fahrenheit = [0, 32, 45, 50, 75, 80, 99, 120];
+
+let celsius = fahrenheit.map((item) => {
+
+    return (item - 32) / 1.8
+
+})
+let roundedC = Math.floor(celsius)
+console.log(roundedC)
+/* ℃ =℉ - 32 ______  1.8000 */
+/* 
+const languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+let sortierung2 = languages.sort()
+
+let sortierung3 = languages.reverse()
+
+console.log(sortierung3)
+//console.log(sortierung2) //(12) ['Swift', 'TypeScript', 'C', 'Go', 'C#', 'CSS', 'C++', 'PHP', 'Ruby', 'Java', 'Python', 'JavaScript']
+
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1);
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed); */
+
+/* let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+let arr2 = numArray2.sort((a, b) => {
+    return a - b
+})
+
+console.log(arr2) */
+
+/* let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+let sort1 = languages.sort()
+console.log(sort1) */
+
